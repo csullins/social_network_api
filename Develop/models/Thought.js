8b186c95504395b14dccae5936833c30ce7e3841
csullins <camilesullins@gmail.com>
2023-06-10 14:03:09 -0500

    adding User and Thought models, config, and file structure

---
+++ Develop/models/Thought.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: function(timestamp) {
+      // Formatting the timestamp using native JavaScript Date object
+      return new Date(timestamp).toLocaleString();
+      }
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    reactions: [reactionSchema]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+  
+  // Virtual property for reactionCount
+  thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+  });
+  
+  // Initialize the Thought model
+  const Thought = model('Thought', thoughtSchema);
+  
+  module.exports = Thought;
